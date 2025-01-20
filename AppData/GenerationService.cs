42c8451886b73a54fc4ed588c14f9aa6930ef0bc
Набойщиков.Алексей <.@TERM1>
2025-01-20 10:27:58 +0300

    Добавлен класс для реализации сервиса генерации пароля.

--- AppData/GenerationService.cs
+++ AppData/GenerationService.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PasswordGenerator1.AppData
+﻿namespace PasswordGenerator1.AppData
 {
     internal class GenerationService
     {
+
     }
 }
