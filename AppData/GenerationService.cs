fd972846c7d54da1d7e7414fd06213d6bc27a8f0
Набойщиков.Алексей <.@TERM1>
2025-01-20 10:09:54 +0300

    Добавьте файлы проекта.

---
+++ AppData/GenerationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator1.AppData
+{
+    internal class GenerationService
+    {
+    }
+}
